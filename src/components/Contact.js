eab1012127022ef8c77fee6c72f9fd8366d8b2a5
idakroniczaja <so.kida@hotmail.com>
2021-08-30 12:39:58 -0400

    email connectin established with emaijs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,12 +4,16 @@ import emailjs from 'emailjs-com';
 
 export default function Contact({contactRef, dataAos}) {
 
-const sendEmail = (e) => {
-    e.prevendDefault(e)
-
-    emailjs.sendForm()
+const sendEmail = (e)=> {
+e.preventDefault()
+ 
+    emailjs.sendForm("service_aowrs29","template_lb3tbjx", e.target ,"user_DSGHm0rdiLjlkKMr2aVXe")
+    .then(res=>{
+        console.log(res.text)
+    }).catch(err=>console.log(err.text))
+    e.target.reset()
 }
-
+  
     return (
         <section data-aos={dataAos}ref={contactRef} id='contact' className='container'>
             <h2>Contact</h2>
@@ -19,10 +23,10 @@ const sendEmail = (e) => {
                  <div id='form'>
                     <form onSubmit={sendEmail}>
                         <h3>Send message</h3>
-                        <input placeholder='your name'></input>
-                        <input placeholder='your email'></input>
-                        <textarea placeholder='message' name="description"  rows="10"></textarea>
-                        <button>Send message</button>
+                        <input name='name' type='text' placeholder='your name'/>
+                        <input name='email' type='email' placeholder='your email'/>
+                        <textarea placeholder='message' name="message"  rows="10"/>
+                        <button type='submit'>Send</button>
                     </form>  
                   </div>
                     
