cc8e727281d2c610ae6b9d6ee96e552170c5e374
idakroniczaja <so.kida@hotmail.com>
2021-09-05 20:44:14 -0400

    navbar styling for desktop version done

---
+++ README.md
@@ -0,0 +1,16 @@
+color pallet:
+
+FONTS:
+#494949 #555 darkslate gray hsl(0,0%,28%) rgb(73,73,73)
+
+
+NAV BAR
+linen #f3ede6 #ffe hsl(32,35%,92%) rgb(243,237,230)
+
+gainsboro #ebe4df #fee hsl(25,23%,89%) rgb(235,228,223)
+
+BURGER MENU
+gray #a19368 #a97 hsl(45,23%,51%) rgb(161,147,104)
+
+
+'BohemeFloral',Helvetica,Arial,Lucida,sans-serif!important
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 
 import { HiMenu } from 'react-icons/hi';
-import { TiTimes } from 'react-icons/ti';
+import { AiOutlineClose } from 'react-icons/ai';
 
 import {useTransition, animated} from 'react-spring';
 import './NavBar.css';
@@ -16,7 +16,7 @@ export default function NavBar({scrollToHeader, scrollToAbout, scrollToProjects,
 const [toggle, setToggle]= useState(false)
 const [windowWidt, setWindowWidth] = useState(window.innerWidth)
 const [lastYPos, setLastYPos] = useState(0)
-const [shouldShowAction, setShouldShowActions] = useState(false)
+const [shouldShowAction, setShouldShowActions] = useState(true)
 
 const transition = useTransition(toggle,(p)=>p,{
     from: {transform: 'scaleY(0)', transformOrigin:'top'},
@@ -51,53 +51,48 @@ useEffect(() => {
 function handleScroll(){
 const yPos = window.scrollY;
 const isScrollingUp = yPos < lastYPos
-
 setShouldShowActions(isScrollingUp)
 setLastYPos(yPos)
-
-
 }
 
   window.addEventListener('scroll', handleScroll, false)
-
 return ()=>{
     window.removeEventListener('scroll', handleScroll, false)
 }
 
-
-  
   },[lastYPos])
 
-
+  
 
     return (
         <motion.nav id='nav-bar' 
-        animate={{height:shouldShowAction ? '100px' : '50px'}} 
-        initial={{height:'100px'}} 
-        transition={{height: {duration:0.5}}}
+        animate={{height:shouldShowAction ? '88px' : '55px', background:shouldShowAction ? 'linear-gradient(#FFFFFF, #FFFFFF)' : 'linear-gradient(#ebe4df, #f3ede6)'}} 
+        initial={{height:'88px', background:'linear-gradient(#FFFFFF, #FFFFFF)'}} 
+        transition={{height: {duration:0.5}, background:{delay:0.3, duration:0.5, transitionTimingFunction: "linear"}}}
         >
 
-        <p>Ida </p>
+        <p>ida</p>
 
         <div id='burger-menu' onClick={()=>setToggle(!toggle)}>
-        {toggle ? <TiTimes /> : <HiMenu />  } 
+        {toggle ? <AiOutlineClose color='#a19368'/> : <HiMenu color='#a19368'/>  } 
             
             
         </div>
-{transition.map(({ item, props, key })=>{
-            return item && <animated.ul style={props}>
-            <li onClick={scrollToHeader}><Link>Home</Link></li>
+{/* {transition.map(({ item, props, key })=>{ */}
+            {/* return item &&  */}
+            <ul>
+            <li onClick={scrollToHeader} ><Link id='homeLink'>Home</Link></li>
             <li onClick={scrollToAbout}><Link>About</Link></li>
             <li onClick={scrollToProjects}><Link>Projects</Link></li>
-            <li onClick={scrollToContact}><Link>Contact</Link></li>
-            </animated.ul>
-         }) 
+            <Link> <motion.li
+            onClick={scrollToContact}
+            id='contactLink'
+            animate={{backgroundColor:shouldShowAction ? '#f3ede6': 'transparent'}}
+            initial={{backgroundColor:'#f3ede6'}}
+            transition={{backgroundColor:{delay:0.3, duration:0.5}}}
+            >Contact</motion.li></Link>
+            </ul>
 
-         
-         
-         
-         }
-        
         </motion.nav>
     )
 }
